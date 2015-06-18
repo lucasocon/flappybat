e2d771a71cd0dd9457f2040bc33f6919d182afb4
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-06-18 14:37:49 -0300

    -add images folder with background and foreground -set up images into window

--- game.rb
+++ game.rb
@@ -1,4 +1,21 @@
 require 'gosu'
 
-window = Gosu::Window.new(800,600,false)
+class GameWindow < Gosu::Window
+	def initialize(*args)
+		super
+		@background = Gosu::Image.new(self, "images/background.png", false)	
+		@foreground = Gosu::Image.new(self, "images/foreground.png", false)
+	end
+
+	def button_down(button)
+		close if button == Gossu::KbEscape
+	end
+
+	def draw
+		@background.draw(0,0,0)
+		@foreground.draw(0,0,0)
+	end
+
+end
+window = GameWindow.new(320,480,false)
 window.show
