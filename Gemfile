690addf63594171ae8f6962d01c42a4a882cf2ad
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-06-18 21:34:21 -0300

    -add defstruct gem and create Game state instance -add player -add gravity

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 
 gem 'gosu', '~> 0.9.2'
+gem 'defstruct', github:'tomdalling/defstruct'
--- game.rb
+++ game.rb
@@ -1,13 +1,23 @@
 require 'gosu'
+require 'defstruct'
+
+GRAVITY = 100 # pixels/s^2
+
+GameState = DefStruct.new{{
+	scroll_x: 0,
+	player_y: 200,
+	player_y_vel: 0,
+	}}
 
 class GameWindow < Gosu::Window
 	def initialize(*args)
 		super
 		@images = {
 			background: Gosu::Image.new(self, "images/background.png", false),
-			foreground: Gosu::Image.new(self, "images/foreground.png", true)
+			foreground: Gosu::Image.new(self, "images/foreground.png", true),
+			player: Gosu::Image.new(self, "images/fruity_1.png", false)
 		}
-		@scroll_x = 0
+		@state = GameState.new
 	end
 
 	def button_down(button)
@@ -15,16 +25,21 @@ class GameWindow < Gosu::Window
 	end
 
 	def update
-		@scroll_x += 3
-		if @scroll_x > @images[:foreground].width
-			@scroll_x = 0
+		@state.scroll_x += 3
+		if @state.scroll_x > @images[:foreground].width
+			@state.scroll_x = 0
 		end
+
+		dt =  update_interval / 1000.0
+		@state.player_y_vel += GRAVITY * dt
+		@state.player_y += @state.player_y_vel * dt
 	end
 
 	def draw
 		@images[:background].draw(0,0,0)
-		@images[:foreground].draw(-@scroll_x,0,0)
-		@images[:foreground].draw(-@scroll_x + @images[:foreground].width,0,0)
+		@images[:foreground].draw(-@state.scroll_x,0,0)
+		@images[:foreground].draw(-@state.scroll_x + @images[:foreground].width,0,0)
+		@images[:player].draw(20,@state.player_y,0)
 	end
 
 end
